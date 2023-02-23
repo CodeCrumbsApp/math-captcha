b12a514c7ad74cbf177dd7bab1e2d2a1268cf99b
Noah Raskin <noahgraskin@gmail.com>
2023-02-23 17:23:59 -0500

    Updates parameters and handles errors

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-;(function (self) {
-	self.mathCaptcha = function ({
+;(function (global) {
+	global.mathCaptcha = function ({
 		formClassSelector,
 		captchaLabelSelector,
 		captchaInputSelector,
-		isValidStyleSelector,
+		targetAttribute = 'valid-captcha-class',
 	}) {
 		const form = document.querySelector(formClassSelector)
 		const submitButton = form.querySelector(
 			'input[type=submit]'
 		)
+
 		submitButton.setAttribute('disabled', 'disabled')
 
 		const captchaLabel = document.querySelector(
@@ -18,11 +19,38 @@
 			captchaInputSelector
 		)
 
+		if (!form) {
+			throw new Error(
+				`No form found with the selector ${formClassSelector}`
+			)
+		}
+
+		if (!submitButton) {
+			throw new Error(
+				`No submit button found on the form with the selector ${formClassSelector}`
+			)
+		}
+
+		if (!captchaLabel) {
+			throw new Error(
+				`No captcha label found with the selector ${captchaLabelSelector}`
+			)
+		}
+
+		if (!captchaInput) {
+			throw new Error(
+				`No captcha input found with the selector ${captchaInputSelector}`
+			)
+		}
+
 		const rndmNr1 = Math.floor(Math.random() * 10)
 		const rndmNr2 = Math.floor(Math.random() * 10)
 		const totalNr = rndmNr1 + rndmNr2
 
-		captchaLabel.innerHtml = `${rndmNr1} + ${rndmNr2} =`
+		if (captchaLabelSelector) {
+			captchaLabel.innerHtml = `${rndmNr1} + ${rndmNr2} =`
+		}
+
 		captchaInput.setAttribute(
 			'placeholder',
 			`${rndmNr1} + ${rndmNr2} =`
@@ -31,11 +59,37 @@
 		captchaInput.addEventListener('keyup', () => {
 			if (captchaInput.value == totalNr) {
 				submitButton.removeAttribute('disabled')
-				captchaInput.classList.add(isValidStyleSelector)
+				addValidClassToElements()
 			} else {
 				submitButton.setAttribute('disabled', 'disabled')
-				captchaInput.classList.remove(isValidStyleSelector)
+				removeValidClassToElements()
 			}
 		})
+
+		const addValidClassToElements = () => {
+			console.log('addValidClassToElements')
+			const elements = document.querySelectorAll(
+				`[${targetAttribute}]`
+			)
+			elements.forEach((element) => {
+				const validClass = element.getAttribute(
+					`${targetAttribute}`
+				)
+				element.classList.add(validClass)
+			})
+		}
+
+		const removeValidClassToElements = () => {
+			console.log('removeValidClassToElements')
+			const elements = document.querySelectorAll(
+				`[${targetAttribute}]`
+			)
+			elements.forEach((element) => {
+				const validClass = element.getAttribute(
+					`${targetAttribute}`
+				)
+				element.classList.remove(validClass)
+			})
+		}
 	}
 })((globalThis.CodeCrumbs = globalThis.CodeCrumbs || {}))
